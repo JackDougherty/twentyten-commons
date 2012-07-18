069cb590c1c9d1ed3b81c350d28e231b5e79c8ef
JackDougherty <jack.dougherty@trincoll.edu>
2012-07-18 12:31:08 -0400

    upload first time

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+	// twenty-ten child theme modified by Jack Dougherty, Trinity College
+	define( 'HEADER_IMAGE_WIDTH', apply_filters( 'twentyten_header_image_width', 940 ) );
+	define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'twentyten_header_image_height', 100 ) );
+
+?>
