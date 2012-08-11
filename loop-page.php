5dbcd601180d6719c703764a1d3bfbfa711595ff
Jack Dougherty <jack.dougherty@trincoll.edu>
2012-08-11 13:28:40 -0400

    modified child theme 2012-08-11
    displays "last updated" date and author on pages
    removed author bio from displaying at bottom of each post

---
+++ loop-page.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The loop that displays a page.
+ *
+ * The loop displays the posts and the post content. See
+ * http://codex.wordpress.org/The_Loop to understand it and
+ * http://codex.wordpress.org/Template_Tags to understand
+ * the tags used in it.
+ *
+ * This can be overridden in child themes with loop-page.php.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.2
+ */
+?>
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php if ( is_front_page() ) { ?>
+						<h2 class="entry-title"><?php the_title(); ?></h2>
+					<?php } else { ?>
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+					<?php } ?> 
+
+<!--refers to published_on child theme function inserted by Jack Dougherty, Trinity College -->
+				<div class="entry-meta">
+						<?php twentyten_published_on(); ?>
+					</div><!-- .entry-meta -->
+
+					<div class="entry-content">
+						<?php the_content(); ?>
+						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
+						<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
+					</div><!-- .entry-content -->
+				</div><!-- #post-## -->
+
+				<?php comments_template( '', true ); ?>
+
+<?php endwhile; // end of the loop. ?>
